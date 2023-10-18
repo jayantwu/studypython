18386ef5069aa5e886956dba58af43e343a1ca4d
“wujiayang” <1064216585@qq.com>
2023-10-18 21:52:09 +0800

    add more j2 test

--- new_study/test7.py
+++ new_study/test7.py
@@ -3,6 +3,7 @@
 import jinja2
 import os
 import json
+from datetime import datetime
 
 print(os.path.dirname(__file__))  # output current path
 
@@ -37,10 +38,23 @@ def test3():
         f.write(buf)
 
 
-
+def test4():
+   path = f"{os.path.dirname(__file__)}/j2"
+   env = jinja2.Environment(
+       loader=jinja2.FileSystemLoader(path),
+       trim_blocks=True,
+       lstrip_blocks=False,
+       extensions=["jinja2.ext.loopcontrols", "jinja2.ext.do"])
+   template = env.get_template(f'2test.j2')
+   template.globals["now"] = datetime.utcnow().strftime("%Y-%m-%d")
+   buf = template.render(age={'ella': 15, 'tony': 19, 'jakey':14}, names=['tony', 'ella', 'jakey'])
+   genfilepath = f"genpy/autogen.txt"
+   with open(genfilepath, 'w') as f:
+       f.write(buf)
 
 
 if __name__ == '__main__':
     testj2()
     test2()
     test3()
+    test4()
