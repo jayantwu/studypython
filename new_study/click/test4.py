e33fb2c41f8a8a5eda51fd766aff64cf4b14b5b7
“wujiayang” <1064216585@qq.com>
2024-04-10 09:42:02 +0800

    update comment

--- new_study/click/test4.py
+++ new_study/click/test4.py
@@ -5,8 +5,8 @@ import sys
 
 def main(cmd_args):
     print("Hello World")
-    sys.argv = ['test_click3.py'] + cmd_args
-    cli()
+    sys.argv = ['test_click3.py'] + cmd_args #修改sys.argv
+    cli() # cli 会读取sys.argv
 
 
 if __name__ == "__main__":
