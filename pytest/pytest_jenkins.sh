248439e6fd36b724c018ae0bcfd14a3103d67c15
“wujiayang” <1064216585@qq.com>
2024-07-06 14:39:57 +0800

    update allure requirement

--- pytest/pytest_jenkins.sh
+++ pytest/pytest_jenkins.sh
@@ -3,7 +3,7 @@
 if [ $# -ne 0 ]; then
     docker run \
     -v /Users/wujiayang/wujiayang/STUDY/docker/study/wujiayang/local_jenkins/jenkins_home/workspace/github_project/pytest_auto/:/var/wujy/ debian-wjy-0207:2.0-cling \
-    bash -c "pip3 install pytest && cd /var/wujy/pytest/ && $@"
+    bash -c "pip3 install pytest && cd /var/wujy/pytest/ pip3 install -r requirements.txt && $@"
 else
     echo "need present prama.."
 fi
